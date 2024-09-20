92f484ddf15cecf63b9b9d61d0c1d10d0a9434c6
Dohaibrahim <doha.ibrahim2003@gmail.com>
2024-09-21 00:14:29 +0300

    Added onboarding screen feature

---
+++ lib/core/widgets/skip_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../theming/styles.dart';
+
+class SkipButton extends StatelessWidget {
+  const SkipButton({super.key,required this.onPressed});
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        TextButton(
+          onPressed: onPressed,
+          child: Text('Skip' , style: TextStyles.font18BlackLight, ),
+        ),
+      ],
+    );
+  }
+}
