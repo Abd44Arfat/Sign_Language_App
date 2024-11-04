fc6366f67dad4ad3185308bdfe64604fc4abee48
abdelrahman arfat <abdoarfat924@gmail.com>
2024-11-04 22:08:49 +0200

    Add scroll on levels and apply multi color in each level

--- lib/features/learn/presentation/levels/levels_view.dart
+++ lib/features/learn/presentation/levels/levels_view.dart
@@ -1,23 +1,33 @@
-import 'package:chat_bubbles/bubbles/bubble_special_one.dart';
 import 'package:flutter/material.dart';
 import 'package:sign_lang_app/core/theming/styles.dart';
 import 'package:sign_lang_app/features/learn/presentation/levels/widgets/Active_level.dart';
 import 'package:sign_lang_app/features/learn/presentation/levels/widgets/non_active_level.dart';
 
-
 class LevelsViewBody extends StatelessWidget {
   final List<String> items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  
+  // Define a list of colors
+  final List<Color> colors = [
+    Color(0xff58CC02),
+    Color(0xffCE82FF),
+    Color(0xff1CB0F6),
+    Color(0xffFF86D0),
+  ];
+    final List<Color> borderColors = [
+    Color(0xff58A700),
+    Color(0xffA568CC),
+    Color(0xff168DC5),
+    Color(0xffCC6BA6),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: Center(
           child: Text(
-            'Categories',
+            'Levels',
             style: TextStyles.font20WhiteSemiBold,
           ),
         ),
@@ -33,55 +43,79 @@ class LevelsViewBody extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: PageView.builder(
+          itemCount: colors.length, // Use the number of colors as the item count
+          scrollDirection: Axis.vertical, // Set to vertical scrolling
+          itemBuilder: (context, index) {
+            return LevelPage(index: index, items: items, colors: colors, borderColors: borderColors,); // Pass the colors list
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class LevelPage extends StatelessWidget {
+  final int index;
+  final List<String> items;
+  final List<Color> colors; 
+    final List<Color> borderColors; 
+
+  // Accept colors list
+
+  LevelPage({required this.index, required this.items, required this.colors, required this.borderColors});
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ActiveLevel(item: items[1],),
-                ],
-              ),
-
-
-
-
-
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.25),
-                child: Row(
-                  children: [
-                    NonActiveLevelWidget(item: items[0]), // Reference to ItemWidget
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.10),
-                child: Row(
-                  children: [
-                    NonActiveLevelWidget(item: items[2]), // Reference to NonActiveItemWidget
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.25),
-                child: Row(
-                  children: [
-                    NonActiveLevelWidget(item: items[3]), // Reference to NonActiveItemWidget
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  NonActiveLevelWidget(item: items[4]), // Reference to NonActiveItemWidget
-                ],
+              ActiveLevel(
+                item: items[index % items.length],
+                backgroundColor: colors[index % colors.length],
+                
+                 borderColor:borderColors[index%borderColors.length] , // Pass the color
               ),
             ],
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.25),
+            child: Row(
+              children: [
+                NonActiveLevelWidget(item: items[(index + 1) % items.length]), // Next level
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.10),
+            child: Row(
+              children: [
+                NonActiveLevelWidget(item: items[(index + 2) % items.length]), // Next level
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.25),
+            child: Row(
+              children: [
+                NonActiveLevelWidget(item: items[(index + 3) % items.length]), // Next level
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              NonActiveLevelWidget(item: items[(index + 4) % items.length]), // Next level
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/features/learn/presentation/levels/widgets/active_level.dart
+++ lib/features/learn/presentation/levels/widgets/active_level.dart
@@ -5,14 +5,20 @@ import 'package:chat_bubbles/chat_bubbles.dart';
 
 class ActiveLevel extends StatelessWidget {
   final String item;
+  final Color backgroundColor; 
+    final Color borderColor; 
+  // New parameter for background color
 
-  ActiveLevel({required this.item,});
+  ActiveLevel({required this.item, required this.backgroundColor, required this.borderColor}); // Constructor updated
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       alignment: Alignment.center,
       children: [
+        // Background color container
+    
+        // Circular progress indicator
         SizedBox(
           width: 110,
           height: 110,
@@ -20,26 +26,22 @@ class ActiveLevel extends StatelessWidget {
             strokeWidth: 8,
             value: 0.3,
             backgroundColor: Color(0xffE5E5E5),
-            valueColor: AlwaysStoppedAnimation<Color>(Color(0xffFFC800)),
+            valueColor: AlwaysStoppedAnimation<Color>(backgroundColor),
           ),
         ),
+        // Animated button
         ChicletOutlinedAnimatedButton(
-          onPressed: (){
-
-
-            
+          onPressed: () {
+            // Action when button is pressed
           },
-          
-          
           height: 70,
           width: 80,
           buttonHeight: 8,
-          backgroundColor: Color(0xff58CC02),
-          borderColor: Color(0xff46A302),
+          backgroundColor:backgroundColor , // Keep button background transparent
+          borderColor: borderColor,
           buttonType: ChicletButtonTypes.oval,
           child: SvgPicture.asset('assets/images/level_icon.svg'),
         ),
-      
       ],
     );
   }
